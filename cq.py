690e8747e6f69a14b0c6adbfcde9923f7bb595c3
dabuc <heyanbo@MacBook-Air.local>
2020-10-13 11:34:00 +0800

    增加导入数据命令

--- cq.py
+++ cq.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """命令工具"""
+from coralquant.etl import import_wm_k_data
 import click
 from coralquant.stock_basic import get_stock_basic
 from coralquant import baostck
 from coralquant.etl import import_daily_k_data
+
 
 @click.group()
 def cli():
@@ -18,28 +20,31 @@ def update_stock_basic():
     get_stock_basic()
     click.echo("A股股票列表更新完成。")
 
+
 @cli.command()
 def init_d_history_k_data_plus():
     """初始化日线数据
     """
     click.confirm("正在初始化日线数据，是否继续？", abort=True)
-    baostck.init_history_k_data_plus('d','tmp_history_A_stock_k_data')
+    baostck.init_history_k_data_plus('d', 'tmp_history_A_stock_k_data')
     click.echo("日线数据初始化完成。")
+
 
 @cli.command()
 def init_w_history_k_data_plus():
     """初始化周线数据
     """
     click.confirm("正在初始化周线数据，是否继续？", abort=True)
-    baostck.init_history_k_data_plus('w','tmp_w_history_A_stock_k_data')
+    baostck.init_history_k_data_plus('w', 'tmp_w_history_A_stock_k_data')
     click.echo("周线线数据初始化完成。")
+
 
 @cli.command()
 def init_m_history_k_data_plus():
     """初始化月线数据
     """
     click.confirm("正在初始化月线数据，是否继续？", abort=True)
-    baostck.init_history_k_data_plus('w','tmp_m_history_A_stock_k_data')
+    baostck.init_history_k_data_plus('w', 'tmp_m_history_A_stock_k_data')
     click.echo("月线数据初始化完成。")
 
 
@@ -51,8 +56,20 @@ def import_daily_data():
     import_daily_k_data.import_data()
     click.echo("ODL日线数据导入完成。")
 
+
+@cli.command()
+@click.option('-f', type=click.Choice(['w', 'm']), prompt=True, help='w：导入周线数据，m：导入月线数据')
+def import_wm_data(f):
+    """导入ODL周月线数据
+    """
+    click.confirm("正在导入导入ODL周月线数据，是否继续？", abort=True)
+    import_wm_k_data.import_data(f)
+    click.echo("ODL周月线数据导入完成。")
+
+
 def main():
     cli()
+
 
 if __name__ == "__main__":
     main()
