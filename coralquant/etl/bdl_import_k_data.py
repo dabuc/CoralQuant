d6d74e77429defe7aa7900a344e1d4fdbc4d12e1
dabuc <heyanbo@MacBook-Air.local>
2020-10-16 12:51:04 +0800

    添加异常失败记录

--- coralquant/etl/bdl_import_k_data.py
+++ coralquant/etl/bdl_import_k_data.py
@@ -86,38 +86,42 @@ def _insert_data(data:list,frequency,pagenum):
     session = get_new_session()
     ins_data=[]
     num = 0 #计数
-    with session_maker(session) as session:
-        for dic in data:   
-            to_table= frequency_to_table[frequency]()
-            to_table.date = dic['date']
-            to_table.code = dic['code']
-            to_table.open = dic['open']
-            to_table.high = dic['high']
-            to_table.low = dic['low']
-            to_table.close = dic['close']
-            to_table.volume = dic['volume']
-            to_table.amount = dic['amount']
-            to_table.adjustflag = dic['adjustflag']
-            to_table.turn = dic['turn']
-            to_table.pctChg = dic['pctChg']
-            if frequency == 'd':
-                to_table.preclose=dic['preclose'] 
-                to_table.tradestatus=dic['tradestatus']
-                to_table.peTTM=dic['peTTM']
-                to_table.pbMRQ=dic['pbMRQ']
-                to_table.psTTM=dic['psTTM']
-                to_table.pcfNcfTTM=dic['pcfNcfTTM']
-                to_table.isST=dic['isST']
-            ins_data.append(to_table)
-            num+=1
-            dm = divmod(num, 1000)
-            if dm[1] == 0:
+    try:
+        with session_maker(session) as session:
+            for dic in data:   
+                to_table= frequency_to_table[frequency]()
+                to_table.date = dic['date']
+                to_table.code = dic['code']
+                to_table.open = dic['open']
+                to_table.high = dic['high']
+                to_table.low = dic['low']
+                to_table.close = dic['close']
+                to_table.volume = dic['volume']
+                to_table.amount = dic['amount']
+                to_table.adjustflag = dic['adjustflag']
+                to_table.turn = dic['turn']
+                to_table.pctChg = dic['pctChg']
+                if frequency == 'd':
+                    to_table.preclose=dic['preclose'] 
+                    to_table.tradestatus=dic['tradestatus']
+                    to_table.peTTM=dic['peTTM']
+                    to_table.pbMRQ=dic['pbMRQ']
+                    to_table.psTTM=dic['psTTM']
+                    to_table.pcfNcfTTM=dic['pcfNcfTTM']
+                    to_table.isST=dic['isST']
+                ins_data.append(to_table)
+                num+=1
+                dm = divmod(num, 1000)
+                if dm[1] == 0:
+                    session.bulk_save_objects(ins_data)
+                    session.commit()
+                    ins_data.clear()
+            if len(ins_data) > 0:
                 session.bulk_save_objects(ins_data)
                 session.commit()
-                ins_data.clear()
-        if len(ins_data) > 0:
-            session.bulk_save_objects(ins_data)
-            session.commit()
+    except Exception as e:
+        _logger.error("第{}页数据导入失败".format(pagenum))
+    else:
         _logger.info("第{}页数据导入完成".format(pagenum))
 
 
