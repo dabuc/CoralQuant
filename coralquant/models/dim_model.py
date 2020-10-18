0a43b941e2bfd468039f99b55db71fa9923bb973
dabuc <heyanbo@MacBook-Air.local>
2020-10-18 12:23:19 +0800

    日期维度添加星期信息

--- coralquant/models/dim_model.py
+++ coralquant/models/dim_model.py
@@ -1,10 +1,12 @@
 from coralquant import logger
 from datetime import date
 from coralquant.database import Base, session_maker
-from sqlalchemy import Integer, Column, Date
+from sqlalchemy import Integer, Column, Date,SmallInteger,String
 import datetime as dt
 
 _logger = logger.Logger(__name__).get_log()
+
+
 class DIM_Date(Base):
     """
     日期维度表
@@ -14,6 +16,9 @@ class DIM_Date(Base):
     date = Column(Date, nullable=False, unique=True)  #年月日
     year_month = Column(Integer, nullable=False, index=True)  #年月
     year = Column(Integer, nullable=False, index=True)  #年
+    weekNum = Column(String(7), nullable=False, index=True)  #所属年份的第几周
+    week= Column(SmallInteger, nullable=False, index=True)
+
 
     @staticmethod
     def init_data():
@@ -22,19 +27,21 @@ class DIM_Date(Base):
         """
         begin_date = dt.datetime.strptime('1990-12-01', '%Y-%m-%d')
         end_date = dt.datetime.strptime('2025-12-31', '%Y-%m-%d')
-        date_list:list[date]=[]
+        date_list: list[date] = []
         while begin_date <= end_date:
             date_list.append(begin_date)
-            begin_date +=dt.timedelta(1)
-        
+            begin_date += dt.timedelta(1)
+
         with session_maker() as sn:
             for tmpdate in date_list:
-                row= DIM_Date(
-                    idate=tmpdate.year*10000+tmpdate.month*100+tmpdate.day,
-                    date=tmpdate,
-                    year_month=tmpdate.year*100+tmpdate.month,
-                    year=tmpdate.year
-                    )
+                row = DIM_Date(idate=tmpdate.year * 10000 + tmpdate.month * 100 + tmpdate.day,
+                               date=tmpdate,
+                               year_month=tmpdate.year * 100 + tmpdate.month,
+                               year=tmpdate.year,
+                               weekNum='{}-{}'.format(tmpdate.year, tmpdate.strftime('%W')),
+                               week = tmpdate.weekday()
+                               )
+
                 sn.add(row)
+
         _logger.info('日期维度表初始化完成')
-        
