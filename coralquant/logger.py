815ca6b1a078f95a14e9f625332d57341adbf416
dabuc <heyanbo@MacBook-Air.local>
2020-10-18 10:09:16 +0800

    日志改为按照文件大小分割

--- coralquant/logger.py
+++ coralquant/logger.py
@@ -4,7 +4,7 @@
 """
 from coralquant.util.barkhttphandler import BarkHTTPHandler
 import logging
-from logging.handlers import TimedRotatingFileHandler
+from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
 import os
 from coralquant.settings import CQ_Config as cfg
 
@@ -22,30 +22,46 @@ class Logger:
             
         self.logger = logging.Logger(logger_name)
         self.logger.setLevel(log_level)
-        
-        # 定义一个按时间：天切分，保留15天的文件日志Handler
-        fh = TimedRotatingFileHandler(
-            log_path,
-            when="D",  # 按天进行切分
-            interval=1,  # 每天都切分
-            backupCount=15,  # 保留15天的日志
-            encoding="utf-8",  # 使用UTF-8的编码来写日志
-            delay=False,
-            utc=False,
-        )  # (log_name,encoding='utf-8')
-        fh.setLevel(log_level)
 
-        ch = logging.StreamHandler()
-        ch.setLevel(log_level)
 
         formatter = logging.Formatter(
             "[%(asctime)s][%(filename)s:%(lineno)d][%(levelname)s][%(thread)d] - %(message)s"
         )
-        fh.setFormatter(formatter)
-        ch.setFormatter(formatter)
+        
+        #按大小分割
+        rfh = RotatingFileHandler(
+            log_path,
+            maxBytes=5242880,#5M
+            backupCount=10,
+            encoding="utf-8"
+        )
+        rfh.setLevel(log_level)
+        rfh.setFormatter(formatter)
+        self.logger.addHandler(rfh)
+        
+        # 定义一个按时间：天切分，保留15天的文件日志Handler
+        # 程序不连续执行，按时间切分是不会成功的。
+        # fh = TimedRotatingFileHandler(
+        #     log_path,
+        #     when='D',  # 按天进行切分
+        #     interval=1,  # 每天都切分
+        #     backupCount=15,  # 保留15天的日志
+        #     encoding="utf-8",  # 使用UTF-8的编码来写日志
+        #     delay=False,
+        #     utc=False,
+        # )
+        # fh.setLevel(log_level)
+        # fh.setFormatter(formatter)
+        # self.logger.addHandler(fh)
 
-        self.logger.addHandler(fh)
+        ch = logging.StreamHandler()
+        ch.setLevel(log_level)
+        ch.setFormatter(formatter)
         self.logger.addHandler(ch)
+
+
+        
+        
 
         if cfg.BARK_HOST != '':
             #定义Bark消息推送服务
