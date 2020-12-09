ddbfb0801af8c653c1bf8162206e88a1a2176f7c
dabuc <heyanbo@MacBook-Air.local>
2020-12-09 23:22:00 +0800

    修改简单的bug

--- coralquant/stringhelper.py
+++ coralquant/stringhelper.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from coralquant.models.bdl_model import DailyKData, DailyKData1, DailyKData2, MonthlyKData, MonthlyKData2, WeeklyKData, WeeklyKData2
-from coralquant.models.odl_model import D1_History_A_Stock_K_Data, D2_History_A_Stock_K_Data, D_History_A_Stock_K_Data, M2_History_A_Stock_K_Data, M_History_A_Stock_K_Data, T52_History_A_Stock_K_Data, T5_History_A_Stock_K_Data, W2_History_A_Stock_K_Data, W_History_A_Stock_K_Data
-from enum import Enum,unique
+from coralquant.models.odl_model import D1_History_A_Stock_K_Data, D2_History_A_Stock_K_Data, D_History_A_Stock_K_Data, M_History_A_Stock_K_Data, W_History_A_Stock_K_Data
+from enum import Enum, unique
+
 
 @unique
 class TaskEnum(Enum):
@@ -19,23 +20,15 @@ class TaskEnum(Enum):
 
     日线历史A股K线后复权数据 = 'd-1'
 
-
     季频盈利能力 = 'profit'
-
 
 
 frequency_odl_table_obj = {
     'd': D_History_A_Stock_K_Data,
     'w': W_History_A_Stock_K_Data,
     'm': M_History_A_Stock_K_Data,
-    '5': T5_History_A_Stock_K_Data,
     'd-2': D2_History_A_Stock_K_Data,
-    'w-2': W2_History_A_Stock_K_Data,
-    'm-2': M2_History_A_Stock_K_Data,
-    '5-2': T52_History_A_Stock_K_Data,
     'd-1': D1_History_A_Stock_K_Data,
-    
-
 }
 
 frequency_bdl_table_obj = {
@@ -46,6 +39,6 @@ frequency_bdl_table_obj = {
     'w-2': WeeklyKData2,
     'm-2': MonthlyKData2,
     'd-1': DailyKData1
-    
+
     #'5': T5_History_A_Stock_K_Data
 }
